108b7b107894a484ec275cc94b01155724ede272
Hassan Outhami <hassan.outhami1@gmail.com>
2022-12-02 16:23:04 +0100

    initial project

---
+++ src/main/java/com/app/spring_security_fundamentals/SpringSecurityFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.app.spring_security_fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/spring_security_fundamentals/SpringSecurityFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.spring_security_fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
