90f8e51acc2fc357bd1b3334c4842a44a2e2dcfe
Hassan Outhami <hassan.outhami1@gmail.com>
2023-01-09 21:41:21 +0100

    add security and basic authentification

--- src/test/java/com/app/spring_security_fundamentals/SpringSecurityFundamentalsApplicationTests.java
+++ src/test/java/com/app/spring_security_fundamentals/SpringSecurityFundamentalsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringSecurityFundamentalsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
